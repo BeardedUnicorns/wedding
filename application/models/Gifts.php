d83493a017db67b38040f426f2f0e54728da5b2c
jbayntun <j_bayntun@hotmail.com>
2015-01-31 12:50:46 -0800

    added some functions to Gifts.php to return the data
    Signed-off-by: jbayntun <j_bayntun@hotmail.com>

--- application/models/Gifts.php
+++ application/models/Gifts.php
@@ -11,4 +11,31 @@ class Gifts extends CI_Model
             array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21),
             array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5)
     );
+    
+     public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function get($id)
+    {
+        foreach ($this->data as $record)
+        {
+            if ($record['giftId'] == $id)
+            {
+                return $record;
+            }
+        }
+        
+        return null;
+    }
+    
+    
+    public function get_all()
+    {
+        return $this->data;
+    }
 }
+
+
+
