82e72ca2d45473edf1d4d0433d434e9291111f05
tyler-hlynsky <tyler.hlynsky@gmail.com>
2015-01-31 14:19:36 -0800

    Comp4711 Assignment 1:
    Added 'location.php' view;
    Added functionality to 'Location.php' controller;
    Added functionality to 'Locations.php' model;
    Signed-off-by:tyler-hlynsky <tyler.hlynsky@gmail.com>

--- application/controllers/Location.php
+++ application/controllers/Location.php
@@ -2,15 +2,25 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Location extends MY_Controller
+class Location extends MY_Controller 
 {
-	public function index()
-	{
-		$this->data['page_body'] = 'location';
-        $this->render();
-	}
-}
 
+    public function __construct() 
+    {
+        parent::__construct();
+        $this->load->model('locations');
+    }
+    
+    public function index() 
+    {
+        $places = $this->locations->get_all();
+        
+        $this->data['page_body'] = 'location';
+        $this->data['places_list'] = $places;
+        $this->render();
+    }
+
+}
 
 /* End of file Location.php */
 /* Location: ./application/controllers/Location.php */
--- application/views/location.php
+++ application/views/location.php
@@ -1,1 +1,11 @@
 <p>Location</p>
+
+<ul>
+    {places_list}
+    <p>{name}</p>
+    {address}
+    {events}
+        <li>{name} - {time}</li>
+    {/events}
+    {/places_list}
+</ul>
