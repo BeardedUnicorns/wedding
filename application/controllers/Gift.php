4bec7600201306fad7c27ac4a65cf91217bdd305
jbayntun <j_bayntun@hotmail.com>
2015-01-31 13:33:00 -0800

    added functionality in Gift controller to display table of gifts
    Signed-off-by: jbayntun <j_bayntun@hotmail.com>

--- application/controllers/Gift.php
+++ application/controllers/Gift.php
@@ -10,11 +10,35 @@ class Gift extends MY_Controller
         $this->load->model('gifts');
     }
     
-    
     public function index()
 	{
-		$this->data['page_body'] = 'gift';
-        $this->render();
+        
+            $gifts = $this->gifts->get_all();
+            $table = '<table>';
+            foreach($gifts as $g)
+            {
+                $table .= '<tr>';
+                
+                $table .= '<td>';
+                $table .= $g['title'];
+                $table .= '</td>';
+                
+                $table .= '<td>';
+                $table .= $g['value'];
+                $table .= '</td>';
+                
+                $table .= '<td>';
+                $table .= $g['description'];
+                $table .= '</td>';
+                
+                $table .= '</tr>';
+            }
+            
+            $table .= '</table>';
+            
+            $this->data['thetable'] = $table;
+            $this->data['page_body'] = 'gift';
+            $this->render();
 	}
 }
 
--- application/models/Gifts.php
+++ application/models/Gifts.php
@@ -5,11 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Gifts extends CI_Model
 {
     private $data = array(
-            array('giftId' => 1, 'title' => 'TV',           'value' => 600),
-            array('giftId' => 2, 'title' => 'HoneyMoon',    'value' => 3000),
-            array('giftId' => 3, 'title' => 'Toaster',      'value' => 36),
-            array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21),
-            array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5)
+            array('giftId' => 1, 'title' => 'TV',           'value' => 600, 'description' => 'great!'),
+            array('giftId' => 2, 'title' => 'HoneyMoon',    'value' => 3000, 'description' => 'Greece!'),
+            array('giftId' => 3, 'title' => 'Toaster',      'value' => 36, 'description' => 'meh'),
+            array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21, 'description' => 'yum'),
+            array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5, 'description' => 'flush')
     );
     
      public function __construct()
