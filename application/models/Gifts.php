447922f014dbd0831e2f40c56daf24b6a336e0c3
jbayntun <j_bayntun@hotmail.com>
2015-01-31 12:44:43 -0800

    Added fake data to the gifts model
    Signed-off-by: jbayntun <j_bayntun@hotmail.com>

--- application/models/Gifts.php
+++ application/models/Gifts.php
@@ -4,5 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Gifts extends CI_Model
 {
-    
+    private $data = array(
+            array('giftId' => 1, 'title' => 'TV',           'value' => 600),
+            array('giftId' => 2, 'title' => 'HoneyMoon',    'value' => 3000),
+            array('giftId' => 3, 'title' => 'Toaster',      'value' => 36),
+            array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21),
+            array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5)
+    );
 }
