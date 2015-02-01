4367b3208548e35869c350d940cc1855b68c493c
Dan Buhler <dan.buehler@outlook.com>
2015-01-31 23:25:39 -0800

    Moved the table creation from the Gift controller into the gift view Signed-off-by:Dan Buhler <dan.buehler@outlook.com>

--- application/controllers/Gift.php
+++ application/controllers/Gift.php
@@ -2,47 +2,33 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * controllers/Gift.php
+ * 
+ * Controller for the Gifts page.
+ */
 class Gift extends MY_Controller
 {
-	public function __construct()
+	/**
+     * Default constructor..
+     */
+    public function __construct()
     {
         parent::__construct();
         $this->load->model('gifts');
     }
     
+    /**
+	 * Index page for this controller.
+     * Gets all the gift items from the Gifts model.
+     */
     public function index()
 	{
-        
-            $gifts = $this->gifts->get_all();
-            $table = '<table id="gift_table">';
-            $table .= '<tr><th>Item</th><th>Cost</th><th>Description</th></tr>';
-            foreach($gifts as $g)
-            {
-                $table .= '<tr>';
-                
-                $table .= '<td>';
-                $table .= $g['title'];
-                $table .= '</td>';
-                
-                $table .= '<td>';
-                $table .= $g['value'];
-                $table .= '</td>';
-                
-                $table .= '<td>';
-                $table .= $g['description'];
-                $table .= '</td>';
-                
-                $table .= '</tr>';
-            }
-            
-            $table .= '</table>';
-            
-            $this->data['thetable'] = $table;
-            $this->data['page_body'] = 'gift';
-            $this->render();
+        $this->data['page_body'] = 'gift';
+        $this->data['gift_items'] = $this->gifts->get_all();
+        $this->render();
 	}
 }
-
 
 /* End of file Gift.php */
 /* Location: ./application/controllers/Gift.php */
--- application/views/gift.php
+++ application/views/gift.php
@@ -1,3 +1,16 @@
 <h2>Gift <span>List</p></h2>
 
-{thetable}
+<table id="gift_table">
+    <tr>
+        <th>Item</th>
+        <th>Cost</th>
+        <th>Description</th>
+    </tr>
+    {gift_items}
+    <tr>
+        <td>{title}</td>
+        <td>{value}</td>
+        <td>{description}</td>
+    </tr>
+    {/gift_items}
+</table>
