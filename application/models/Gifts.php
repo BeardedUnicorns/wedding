b18c3222d95b5ba7e09e6c6aa867ec9a35c64883
tyler-hlynsky <tyler.hlynsky@gmail.com>
2015-01-31 23:21:43 -0800

    Updated colours for Guest and Gift tables; Re-arranged radio buttons on view and added pre-set values corresponding to response; Added checkboxes to Gift table; Signed-off-by:tyler-hlynsky <tyler.hlynsky@gmail.com>

--- application/models/Gifts.php
+++ application/models/Gifts.php
@@ -5,11 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Gifts extends CI_Model
 {
     private $data = array(
-            array('giftId' => 1, 'title' => 'TV',           'value' => 600, 'description' => 'great!'),
-            array('giftId' => 2, 'title' => 'HoneyMoon',    'value' => 3000, 'description' => 'Greece!'),
-            array('giftId' => 3, 'title' => 'Toaster',      'value' => 36, 'description' => 'meh'),
-            array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21, 'description' => 'yum'),
-            array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5, 'description' => 'flush')
+            array('giftId' => 1, 'title' => 'TV',           'value' => 600, 'description' => 'great!', 'checkbox' => '<input type="checkbox" name="c1">'),
+            array('giftId' => 2, 'title' => 'HoneyMoon',    'value' => 3000, 'description' => 'Greece!', 'checkbox' => '<input type="checkbox" name="c2">'),
+            array('giftId' => 3, 'title' => 'Toaster',      'value' => 36, 'description' => 'meh', 'checkbox' => '<input type="checkbox" name="c3">'),
+            array('giftId' => 4, 'title' => 'Fruit Basket', 'value' => 21, 'description' => 'yum', 'checkbox' => '<input type="checkbox" name="c4">'),
+            array('giftId' => 5, 'title' => 'Goldfish',     'value' => 5, 'description' => 'flush', 'checkbox' => '<input type="checkbox" name="c5">')
     );
     
      public function __construct()
