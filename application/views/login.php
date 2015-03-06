0c1371bb0c12320dbc2919f594adc3106e4d041f
Dan Buhler <dan.buehler@outlook.com>
2015-03-05 16:25:49 -0800

    Added link to login as admin Signed-off-by:Dan Buhler <dan.buehler@outlook.com>

--- application/views/login.php
+++ application/views/login.php
@@ -1,3 +1,4 @@
 <h2>Login</h2>
 
-<p>Click <a href="/">here</a> to log in.</p>
+<p>Click <a href="/">here</a> to log in as a normal user.</p>
+<p>Click <a href="/admin">here</a> to log in with admin rights.</p>
