817e71d11bb8b206ab475cf548efc3fa0103f242
jbayntun <j_bayntun@hotmail.com>
2015-03-06 11:41:43 -0800

    autoloaded models
    added functions to groups, users
    
    Signed-off-by: jbayntun <j_bayntun@hotmail.com>

--- application/models/Guests.php
+++ application/models/Guests.php
@@ -5,58 +5,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * models/Guests.php
  * 
- * Model for guest groups.
+ * Model for guest groups.  Corresponds to the groups table.
  */
-class Guests extends CI_Model
-{
-    private $data = array(
-        array('id' => '1', 'name' => 'The Starks', 'comments' => 'No comment',
-            'responses' => array(
-                array('name' => 'Eddard Stark',  'status' => 'Cannot attend', 'radio' => '<input type="radio" name="r1"><input type="radio" name="r1"><input type="radio" name="r1">'),
-                array('name' => 'Catelyn Tully', 'status' => 'Will attend',   'radio' => '<input type="radio" name="r2"><input type="radio" name="r2"><input type="radio" name="r2">'),
-                array('name' => 'Robb Stark',    'status' => 'Cannot attend', 'radio' => '<input type="radio" name="r3"><input type="radio" name="r3"><input type="radio" name="r3">'),
-                array('name' => 'Sansa Stark',   'status' => 'Will attend',   'radio' => '<input type="radio" name="r4"><input type="radio" name="r4"><input type="radio" name="r4">'),
-                array('name' => 'Bran Stark',    'status' => 'Not sure yet',  'radio' => '<input type="radio" name="r5"><input type="radio" name="r5"><input type="radio" name="r5">'),
-                array('name' => 'Arya Stark',    'status' => 'Cannot attend', 'radio' => '<input type="radio" name="r6"><input type="radio" name="r6"><input type="radio" name="r6">'),
-                array('name' => 'Rickon Stark',  'status' => 'Will attend',   'radio' => '<input type="radio" name="r7"><input type="radio" name="r7"><input type="radio" name="r7">'))
-        )
-    );
-    
-    /**
-     * Default constructor.
-     */
-    public function __construct()
+class Guests extends MY_Model
+{    
+        // Constructor
+    public function __construct() {
+       parent::__construct('groups', 'id');	
+    }
+
+    public function get_users($key)
     {
-        parent::__construct();
+        $ci = &get_instance();
+        return $ci->users->getGroup($key);
     }
     
-    /**
-     * Returns the guest group with the given ID.
-     * 
-     * @param type $id a group ID
-     * @return type the group with this ID 
-     */
-    public function get($id)
+    public function add_user($group_id, $user)
     {
-        foreach ($this->data as $record)
-        {
-            if ($record['id'] == $id)
-            {
-                return $record;
-            }
-        }
-        
-        return null;
-    }
-    
-    /**
-     * Returns all guest groups.
-     * 
-     * @return type all groups
-     */
-    public function get_all()
-    {
-        return $this->data;
+        $ci = &get_instance();
+        $user['group_id'] = $group_id;
+        $ci->users->add($user);        
     }
 }
 
