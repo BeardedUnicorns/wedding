dcef2f8f61082f65ce766b7dcadd515113066f96
Dan Buhler <dan.buehler@outlook.com>
2015-03-06 10:21:24 -0800

    Autoloaded database library and added username root to db config Signed-off-by:Dan Buhler <dan.buehler@outlook.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -96,7 +96,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('parser', 'session');
+$autoload['libraries'] = array('database', 'parser', 'session');
 
 
 /*
