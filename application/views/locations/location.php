8efd2bacc57af53d151d1bb89f7b0ab228e12139
tyler-hlynsky <tyler.hlynsky@gmail.com>
2015-03-08 23:49:35 -0700

    Assignment 2: Wedding
    1. More CSS for Locations.
    Signed-off-by:tyler-hlynsky <tyler.hlynsky@gmail.com>

--- application/views/locations/location.php
+++ application/views/locations/location.php
@@ -1,13 +1,22 @@
 <h2>Wedding <span>Location</span></h2>
 
-<ul>
+<table class="blue_table">
     {places_list}
-        <p>{event_title}</p>
-        {description}
-        {start_time}
-        {notes}
-        {address}
-        {html}
-        <hr>
+    <tr>
+        <th>Event</th>
+        <th>Description</th>
+        <th>Start Time</th>
+        <th>Address</th>
+    </tr>
+    <tr>
+        <td>{event_title}</td>
+        <td>{description}</td>
+        <td>{start_time}</td>
+        <td>{address}</td>
+</table>
+<div id="notes">
+    <h4>Notes:</h4>
+        <p>{notes}</p>
+</div>
+    {html}
     {/places_list}
-</ul>
