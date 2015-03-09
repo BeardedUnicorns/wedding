ae10b1932d5e485acfdf7f809ed2bba7ac9e9abd
tyler-hlynsky <tyler.hlynsky@gmail.com>
2015-03-08 23:44:17 -0700

    Assignment 2: Wedding
    1. Location CSS brought in line with the rest of the styling.
    Signed-off-by:tyler-hlynsky <tyler.hlynsky@gmail.com>

--- application/views/locations/location_admin.php
+++ application/views/locations/location_admin.php
@@ -2,13 +2,28 @@
 
 <p><a href="/location/add_location">Add New Location</a></p>
 
-<ul>
+<table class="blue_table">
     {places_list}
-    <a href="/location/edit_location/{id}">{event_title}</a>
-    {description}
-    {start_time}
-    {notes}
-    {address}
+    <tr>
+        <th>Event</th>
+        <th>Description</th>
+        <th>Start Time</th>
+        <th>Address</th>
+        <th class="admin">Admin</th>
+    </tr>
+    <tr>
+        <td>{event_title}</td>
+        <td>{description}</td>
+        <td>{start_time}</td>
+        <td>{address}</td>
+        <td>
+            <a href="/location/edit_location/{id}">Edit</a>
+            <a href="/location/delete_location/{id}">Delete</a>
+        </td>
+</table>
+<div id="notes">
+    <h4>Notes:</h4>
+        <p>{notes}</p>
+</div>
     {html}
     {/places_list}
-</ul>
