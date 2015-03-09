a54d2858dcebf15e8563142228d56b2d37050150
Dan Buhler <dan.buehler@outlook.com>
2015-03-09 00:56:27 -0700

    Final CSS adjustments and minor fixes in locations stuff Signed-off-by:Dan Buhler <dan.buehler@outlook.com>

--- application/views/locations/add_location.php
+++ application/views/locations/add_location.php
@@ -1,7 +1,6 @@
-<h2>Location <span>Add</span></h2>
+<h2>Add <span>Location</span></h2>
 
-<form class="location_form" action="/location/submit_new_location/" method="post">
-    <h3>Location <span>Info</span></h3>
+<form class="location_form" action="/location/submit_new_location" method="post">
     <div>
         <label for="event_title">Event Name</label>
         <input type="text" id="event_title" name="event_title">
@@ -15,16 +14,16 @@
         <input type ="text" id="start_time" name="start_time">
     </div>
     <div>
-        <label for="notes">Notes</label>
-        <input type="text" id="notes" name="notes">
-    </div>
-    <div>
         <label for="address">Address</label>
         <input type="text" id="address" name="address">
     </div>
     <div>
-        <label for="html">HTML</label>
-        <input type="text" id="html" name="html">
+        <label for="html">HTML:</label>
+        <textarea id="html" name="html"></textarea>
+    </div>
+    <div>
+        <label for="notes">Notes:</label>
+        <textarea id="notes" name="notes"></textarea>
     </div>
     <input type="submit" value="Add Location">
 </form>
--- application/views/locations/edit_location.php
+++ application/views/locations/edit_location.php
@@ -1,6 +1,6 @@
-<h2>Location <span>Edit</span></h2>
+<h2>Edit <span>Location</span></h2>
 
-<form action="/location/submit_location/{id}" method="post">
+<form class="location_form" action="/location/submit_location/{id}" method="post">
     <div>
         <label for="event_title">Event Name</label>
         <input type="text" id="event_title" name="event_title" value="{event_title}">
@@ -14,16 +14,16 @@
         <input type ="text" id="start_time" name="start_time" value='{start_time}'>
     </div>
     <div>
-        <label for="notes">Notes</label>
-        <input type="text" id="notes" name="notes" value="{notes}">
-    </div>
-    <div>
         <label for="address">Address</label>
         <input type="text" id="address" name="address" value="{address}">
     </div>
     <div>
-        <label for="html">HTML</label>
-        <input type="text" id="html" name="html" value="{html}">
+        <label for="html">HTML:</label>
+        <textarea id="html" name="html">{html}</textarea>
     </div>
-    <input type="submit" value="Edit Location">
+    <div>
+        <label for="notes">Notes:</label>
+        <textarea id="notes" name="notes">{notes}</textarea>
+    </div>
+    <input type="submit" value="Save Changes">
 </form>
